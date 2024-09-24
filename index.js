c526acd78bd27974c72124870b6a1a1eaaf0b275
Luis Guerra <luis910gg@gmail.com>
2024-09-23 22:45:42 -0400

    Exercise 3.11 completed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("content", (request, response) => {
   const data = request.body;
