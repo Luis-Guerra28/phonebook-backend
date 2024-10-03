ff24b3dd3767808de0ef57208099871086dbbb5c
Luis Guerra <luis910gg@gmail.com>
2024-10-03 19:55:40 -0400

    Add API function to get contact by ID

--- index.js
+++ index.js
@@ -76,19 +76,18 @@ app.post("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
-  console.log(request.params.id);
-
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
